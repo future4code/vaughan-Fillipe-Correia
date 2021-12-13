fc90932b269f4bfd727667d9d6cb04e4caa2209f
FillipeCO <flip_dias@hotmail.com>
2021-12-13 16:44:03 -0300

    adicionando o exercicio 12 da revisão-js ao repositório

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -145,7 +145,8 @@ function retornaChamadaDeFilme(filme) {
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+    pessoa.nome = "ANÔNIMO";
+    return pessoa;
 }
 
 // EXERCÍCIO 13A
