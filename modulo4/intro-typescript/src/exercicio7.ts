1660fcb6d5fcc38607b79b941df552a0f2537c73
FillipeCO <flip_dias@hotmail.com>
2022-03-23 17:49:38 -0300

    exercicio intro typescript

---
+++ modulo4/intro-typescript/src/exercicio7.ts
@@ -0,0 +1,17 @@
+const dnaToRna = (dna: string): string => {
+    let rna = ""
+    for (let i = 0; i < dna.length; i++) {
+        if (dna[i] === "A") {
+            rna += "U"
+        } else if (dna[i] === "T") {
+            rna += "A"
+        } else if (dna[i] === "G") {
+            rna += "C"
+        } else if (dna[i] === "C") {
+            rna += "G"
+        }
+    }
+    return rna  
+}
+
+console.log(dnaToRna("ATTGCTGCGCATTAACGACGCGTA"));
